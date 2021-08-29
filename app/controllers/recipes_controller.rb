e52fb69aeef5f681d8723e7b9312c50e31de5f4c
Hannah Malm <hannahemalm616@gmail.com>
2021-08-29 09:19:26 -0400

    commit tree 10615f841716817df8b65a80f4a44a2973ab4ce0

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,10 +7,27 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  #This will render the new view which has the first and second ingredients
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "First Ingredient")
+    @recipe.ingredients.build(name: "Second Ingredient")
   end
 
+
+  #this will create recipies and redirect them 
   def create
+    Recipe.create(recipe_params)
+    redirect_to recipes_path
   end
+
+
+  #this is permitting you to change the title and the ingredient name and quantity
+  #remember to change this and the new field when updating
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [:name, :quantity]
+    )
+  end 
 end
